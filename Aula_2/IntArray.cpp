fe22369d63d5240555d8a9a71d379b623c8f3ddd
Bruno Kakazu <43326668+BrunoKakazu@users.noreply.github.com>
2023-03-09 17:20:24 -0400

    Create IntArray.cpp

---
+++ Aula_2/IntArray.cpp
@@ -0,0 +1,10 @@
+Tarefa 2
+   
+######################################################################################################################################################################################################################################################################################################################
+
+#include "IntArray.h"
+
+void IntArray::resize()		// :: -> operador de escopo, significa que ele serve para qualificar nomes. Um nome qualificado é precedido de um escopo, ou seja, ele está em algum outro lugar
+{
+
+}
